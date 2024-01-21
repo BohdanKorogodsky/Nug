8362b74d307dc7e6906cecc1f5397a1c88283049
Bohdan Korogodsky <bogdankorogodskij@gmail.com>
2024-01-21 17:25:29 +0300

    +

--- routes/web.php
+++ routes/web.php
@@ -53,3 +53,68 @@ Route::match(['get', 'post'], '/', function () {
     // Обслуживаем запросы GET или POST по этому маршруту
 });
 
+Route::get('members/{id}', 'MembersController@show')->name('members.show');
+
+Route::get('members/{id}', [
+    'as' => 'members.show',
+    'uses' => 'MembersController@show',
+]);
+
+Route::middleware('auth:api', 'throttle:60,1')->group(function () {
+    Route::get('/profile', function () {
+
+    });
+});
+
+Route::prefix('dashboard')->group(function () {
+    Route::get('/', function () {
+        // Обрабатывает путь /dsahboard
+    });
+    Route::get('users', function () {
+        // Обрабатывает путь /dsahboarb/users
+    });
+});
+
+Route::any('{anything}', 'CatchAllController')->where('anything', '*');
+
+Route::fallback(function () {
+    //
+});
+
+Route::domain('api.myapp.com')->group(function () {
+    Route::get('/', function () {
+        //
+    });
+});
+
+Route::domain('{account}.myapp.com')->group(function () {
+    Route::get('/', function ($account) {
+        //
+    });
+    Route::get('users/{id}', function ($account, $id) {
+        //
+    });
+});
+
+Route::name('users.')->prefix('users')->group(function () {
+    Route::name('comments.')->prefix('comments')->group(function () {
+        Route::get('{id}', function () {
+
+        })->name('show');
+    });
+});
+
+Route::get('invitations/{invotation}/{answer}', 'InvitationController')->name('invitations');
+
+// Генерирование нормальной ссылки
+URL::route('invitations', ['invitation' => 12345, 'answer' => 'yes']);
+
+// Генерирование подписанной ссылки
+URL::signedRoute('invitations', ['invitation' => 12345, 'answer' => 'yes']);
+
+// Генерирование подписанной ссылки с ограниченным сроком действия (временной)
+URL::temporaryRouteSignedRoute(
+    'invitations',
+    now()->addHours(4),
+    ['invitation' => 12345, 'answer' => 'yes']
+);
